5618fafb449bfbcb85fc0653ea320b66dacf6cca
Pritam Sarkar <sarkarpritam0007@gmail.com>
2024-07-24 12:51:38 +0530

    EC136 & EC137 UI designed

--- src/app/core/services/stamp/stamp-reports.service.ts
+++ src/app/core/services/stamp/stamp-reports.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ToastService } from '../toast.service';
+import { IapiResponce } from '../../models/iapi-responce';
+import { Observable, catchError } from 'rxjs';
+import { AddVendorStampRequisition, ApprovedByClerk, ApprovedByTO, GetVendorStampRequisition, PrintData } from '../../models/stamp';
+import { DynamicTableQueryParameters } from '../../models/dynamic-table';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StampReportsService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private toastService: ToastService) { }
 }
