4238ecc7ba21004d9d8397ded92063b521bc3505
Pritam Sarkar <sarkarpritam0007@gmail.com>
2024-08-13 15:43:36 +0530

    Stamp damage details UI card added

---
+++ src/app/features/stamp-strong-room/stamp-damage-details/stamp-damage-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stamp-damage-details',
+  templateUrl: './stamp-damage-details.component.html',
+  styleUrls: ['./stamp-damage-details.component.scss']
+})
+export class StampDamageDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
