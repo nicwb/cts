b21ffd14516bbddda4dc39df05f443ff52539755
shruti <shruti040112@gmail.com>
2024-09-02 10:33:28 +0530

    Write Playwright tests for Pension Subcategory Module
    Write Playwright tests for Pension Subcategory Module
    
    Write Playwright tests for Pension Subcategory Module

--- e2e/subcategory.spec.ts
+++ e2e/subcategory.spec.ts
@@ -1,31 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('SubCategory Details Module', () => {
-    test.beforeEach(async ({ page, isMobile }) => {
-        test.fixme(isMobile, "Complete task-142 before runnign this test");
+    test.beforeEach(async ({ page }) => {
         // Navigate to the static login page containing user roles
         await page.goto('/#/static-login');
         await page.getByRole('link', { name: 'cleark' }).click();
-        await expect(page.getByText('CCTSCLERK')).toBeVisible();
+        await expect(page.getByRole('link', { name: 'logo | CTS' })).toBeVisible();
         await page.goto('/#/master/app-pension/app-sub-category');
-    });
-
-    test('Check the "New Entry" button is visible or not', async ({ page }) => {
-        await expect(page.getByRole('button', { name: 'New' })).toBeVisible();
     });
 
     test('Check the input box is visible and working or not and submit it successfully', async ({
         page,
     }) => {
-        let num = Math.floor(Math.random() * 1000 - 999 + 1) + 999;
         await page.getByRole('button', { name: 'New' }).click();
-        await expect(
-            page.getByLabel('Sub-Caregory Details').getByRole('textbox')
-        ).toBeVisible();
-        await page
-            .getByLabel('Sub-Caregory Details')
-            .getByRole('textbox')
-            .fill(`ROPA ${num} `);
         await expect(
             page.getByRole('button', { name: 'Submit' })
         ).toBeVisible();
@@ -35,16 +22,11 @@ test.describe('SubCategory Details Module', () => {
         );
     });
 
+    
+
     test('Duplicate Data Checking ', async ({ page }) => {
-        let num = Math.floor(Math.random() * 1000 - 999 + 1) + 999;
         await page.getByRole('button', { name: 'New' }).click();
-        await expect(
-            page.getByLabel('Sub-Caregory Details').getByRole('textbox')
-        ).toBeVisible();
-        await page
-            .getByLabel('Sub-Caregory Details')
-            .getByRole('textbox')
-            .fill(`ROPA ${num} `);
+        const data = await page.locator('input[formControlName=SubCategoryName]').inputValue();
         await expect(
             page.getByRole('button', { name: 'Submit' })
         ).toBeVisible();
@@ -53,13 +35,7 @@ test.describe('SubCategory Details Module', () => {
             'Sub Category Details added successfully'
         );
         await page.getByRole('button', { name: 'New' }).click();
-        await expect(
-            page.getByLabel('Sub-Caregory Details').getByRole('textbox')
-        ).toBeVisible();
-        await page
-            .getByLabel('Sub-Caregory Details')
-            .getByRole('textbox')
-            .fill(`ROPA ${num} `);
+        await page.locator('input[formControlName=SubCategoryName]').fill(data);
         await expect(
             page.getByRole('button', { name: 'Submit' })
         ).toBeVisible();
@@ -67,18 +43,5 @@ test.describe('SubCategory Details Module', () => {
         await expect(page.getByRole('alert')).toContainText(
             'This already exsists.'
         );
-    });
-
-    test('Check the cancel button is visible and working or not', async ({
-        page,
-    }) => {
-        await page.getByRole('button', { name: 'New' }).click();
-        await expect(
-            page.getByRole('button', { name: 'Cancel' })
-        ).toBeVisible();
-        await page.getByRole('button', { name: 'Cancel' }).click();
-        await expect(
-            page.getByLabel('Sub-Caregory Details').getByRole('textbox')
-        ).toBeHidden();
     });
 });
