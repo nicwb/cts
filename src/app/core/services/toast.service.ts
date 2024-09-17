34ea278c102716c6eeae8d62de0d94674dda5b0a
Abu Salam [Lenovo-Slim3i] <abu.alam@nic.in>
2024-09-17 07:39:16 +0530

    Use swal instead of primeng toast

--- src/app/core/services/toast.service.ts
+++ src/app/core/services/toast.service.ts
@@ -1,40 +1,56 @@
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import Swal from 'sweetalert2';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ToastService {
     constructor(private messageService: MessageService) { }
-    showAlert(message: string,alertType:number){
+    showAlert(message: string, alertType: number){
         switch (alertType) {
-        case 1:
+        case AlertType.Success:
             this.showSuccess(message);
             break;
-        case 2:
+        case AlertType.Warning:
             this.showWarning(message);
             break;      
-        case 3:
+        case AlertType.Error:
             this.showError(message);
             break;
-        case 4:
+        case AlertType.Info:
             this.showInfo(message);
             break;
         default:
             break;
         }
     }
-    showSuccess(message: string) {
-        this.messageService.add({ severity: 'success', summary: 'Success', detail: message });
+    showSuccess(message: string, swal: boolean = true) {
+        swal
+            ? Swal.fire({ icon: 'success', title: 'Success', text: '' + message })
+            : this.messageService.add({ severity: 'success', summary: 'Success', detail: message });
     }
+    
+    showError(message: string, swal: boolean = true) {
+        swal
+            ? Swal.fire({ icon: 'error', title: 'Aww! Snap...', text: '' + message })
+            : this.messageService.add({ severity: 'error', summary: 'Error', detail: message });
+    }
+    showWarning(message: string, swal: boolean = true) {
+        swal
+            ? Swal.fire({ icon: 'warning', title: 'Heads up...', text: '' + message })
+            : this.messageService.add({ severity:'warn', summary: 'Warn', detail: message });
+    }
+    showInfo(message: string, swal: boolean = true) {
+        swal
+            ? Swal.fire({ icon: 'info', title: 'Info', text: '' + message })
+            : this.messageService.add({ severity:'info', summary: 'Info', detail: message });
+    }
+}
 
-    showError(message: string) {
-        this.messageService.add({ severity: 'error', summary: 'Error', detail: message });
-    }
-    showWarning(message: string) {
-        this.messageService.add({ severity:'warn', summary: 'Warn', detail: message });
-    }
-    showInfo(message: string) {
-        this.messageService.add({ severity:'info', summary: 'Info', detail: message });
-    }
+export enum AlertType {
+    Success = 1,
+    Warning = 2,
+    Error   = 3,
+    Info    = 4
 }
