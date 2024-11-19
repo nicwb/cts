b710df265080ed5b5c039d20b7105c1b6d643644
Nilakshi Chakraborty <chakraborty.nilakshi2004@gmail.com>
2024-11-19 16:20:14 +0530

    Create life time arrear pension bill
    Create life time arrear pension bill

---
+++ src/app/features/pensions-process/pension-bill/life-time-arrear-pension-bill/life-time-arrear-pension-bill.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { PensionPPODetailsService } from 'src/app/api';
+
+@Component({
+    selector: 'app-life-time-arrear-pension-bill',
+    templateUrl: './life-time-arrear-pension-bill.component.html',
+    styleUrls: ['./life-time-arrear-pension-bill.component.scss']
+})
+export class LifeTimeArrearPensionBillComponent implements OnInit {
+
+    allManualPPOReceipt$?: Observable<any>;
+    lifeTimeArrearPensionBillForm: FormGroup = new FormGroup({});
+    constructor(
+        private ServiceForId: PensionPPODetailsService,
+        private fb: FormBuilder
+    ) {}
+    ngOnInit(): void {
+        this.allManualPPOReceipt$ = this.ServiceForId.getPensioners();
+        this.ininalizer();
+    }
+    ininalizer() {
+        this.lifeTimeArrearPensionBillForm = this.fb.group({
+            ppoId: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+            ppoSlNo: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+            ppoNo: ['', [Validators.required, Validators.maxLength(100)]],
+            ppoName: ['', Validators.maxLength(100)],
+            periodFrom: ['', Validators.required],
+            periodTo: ['', Validators.required],
+            bankName: ['', Validators.required],
+            bankAcNo: ['', Validators.required]
+        });
+    }
+    onRefresh(): void {
+        this. lifeTimeArrearPensionBillForm.reset();
+    }
+    onGenerate() {
+        console.log("Generation Button is clicked");
+    }
+    async handelManualEntrySelect(event: any) {
+        console.log(event);
+    }
+    async saveData() {
+        console.log("Save Button is clicked");
+    }
+
+}
--- src/app/features/pensions-process/pension-bill/pension-bill.module.ts
+++ src/app/features/pensions-process/pension-bill/pension-bill.module.ts
@@ -6,9 +6,19 @@ import {FirstPensionBillModule} from './first-pension-bill/first-pension-bill.mo
 import { first } from 'rxjs';
 import { RouterModule, Routes } from '@angular/router';
 import { OptionCardModule } from 'src/app/shared/modules/option-card/option-card.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { CommonHeaderModule } from 'src/app/shared/modules/common-header/common-header.module';
+import { DynamicTableModule } from 'src/app/shared/modules/dynamic-table/dynamic-table.module';
+import { TreasuryDropdownModule } from 'src/app/shared/modules/treasury-dropdown/treasury-dropdown.module';
 import { FirstPensionBillComponent } from './first-pension-bill/first-pension-bill.component';
 import { RegularPensionBillComponent } from './regular-pension-bill/regular-pension-bill.component';
 import { RegularPensionBillModule } from './regular-pension-bill/regular-pension-bill.module';
+import { LifeTimeArrearPensionBillComponent } from './life-time-arrear-pension-bill/life-time-arrear-pension-bill.component';
+import { LifeTimeArrearPensionBillModule } from './life-time-arrear-pension-bill/life-time-arrear-pension-bill.module';
 
 type NewType = Routes;
 
@@ -25,6 +35,11 @@ const routes: NewType = [
         path: 'regular-pension-bill',
         component: RegularPensionBillComponent,
         data: { breadcrumb: 'RegularPensionBillComponent'}
+    },
+    {
+        path: 'life-time-arrear-pension-bill',
+        component: LifeTimeArrearPensionBillComponent,
+        data: { breadcrumb: 'LifeTimeArrearPensionBillComponent'}
     }
 ];
 @NgModule({
@@ -32,8 +47,18 @@ const routes: NewType = [
     imports: [
         CommonModule,
         OptionCardModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ButtonModule,
+        CalendarModule,
+        DialogModule,
+        DropdownModule,
+        CommonHeaderModule,
+        DynamicTableModule,
+        TreasuryDropdownModule,
         RegularPensionBillModule,
         FirstPensionBillModule,
+        LifeTimeArrearPensionBillModule,
         RouterModule.forChild(routes)
     ],
     exports: [RouterModule] // Export if needed in other modules
