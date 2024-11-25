caeb43433198efe991221cb067a6ebaf0d5bd5f1
Nilakshi Chakraborty <chakraborty.nilakshi2004@gmail.com>
2024-11-25 13:04:27 +0530

    Fix Breadcrumbs navigation is not using baseUrl
    Fix Breadcrumbs navigation is not using baseUrl

--- src/app/layout/breadcrumb/breadcrumb.component.ts
+++ src/app/layout/breadcrumb/breadcrumb.component.ts
@@ -3,20 +3,24 @@ import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { BreadcrumbService } from 'src/app/layout/service/breadcrumb-service.service';
 import { LayoutService } from '../service/app.layout.service';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
     selector: 'app-breadcrumb',
     templateUrl: './breadcrumb.component.html',
 })
 export class BreadcrumbComponent implements OnInit, OnDestroy {
-    breadcrumbs: Array<{ label: string, url: string }> = [];
+    breadcrumbs: MenuItem[] = [];
     navigationSubscription: Subscription;
     viewportWidth: number = window.innerWidth;
+    home: MenuItem = { icon: 'pi pi-home', routerLink: '/' };
 
     constructor(
         public layoutService: LayoutService,
         private breadcrumbService: BreadcrumbService,
-        private location: Location
+        private location: Location,
+        public router: Router
     ) {
         this.navigationSubscription = this.breadcrumbService.routeChangeHandler$
             .subscribe(() => {
@@ -39,9 +43,28 @@ export class BreadcrumbComponent implements OnInit, OnDestroy {
     }
 
     updateBreadcrumbs() {
-        const allBreadcrumbs = this.breadcrumbService.breadcrumbs;
-        // Display the last two breadcrumbs if width <= 678px
-        this.breadcrumbs = this.viewportWidth <= 678 ? allBreadcrumbs.slice(-2) : allBreadcrumbs;
+        const allBreadcrumbs = this.breadcrumbService.breadcrumbs.map((crumb, index) => {
+            return {
+                label: crumb.label,
+                routerLink: crumb.url,
+                command: (event: any) => {
+                    event.originalEvent?.preventDefault();
+
+                    // If clicking the current page's breadcrumb, do nothing
+                    if (this.router.url === crumb.url) {
+                        return;
+                    }
+
+                    // Otherwise, navigate to the clicked breadcrumb's page
+                    this.router.navigate([crumb.url])
+                        .catch(() => this.location.back());
+                }
+            };
+        });
+
+        this.breadcrumbs = this.viewportWidth <= 678 ?
+            allBreadcrumbs.slice(-2) :
+            allBreadcrumbs;
     }
 
     pageReload() {
